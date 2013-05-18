ef6b23a9455a8796f5a38f8457a4704f9a3b7ce9
Michael Raimondi <zeno@pobox.com>
2013-05-18 11:55:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogorama::Application.config.secret_token = 'a7d13998363d0f33b2a78e2eef766b9f4231123339fb19d40894087153ef34d462a61e5ed787e5497dd358dc414f0c05c6af57edd15e5c2b2d6ee071567a6e74'
