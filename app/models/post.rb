e456df78cddcc36bdc30e6a0563b27e66af7b316
Michael Raimondi <zeno@pobox.com>
2013-05-18 12:34:35 -0400

    Add blog post model with basic validations

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Base
+  attr_accessible :body, :title
+
+  validates_presence_of :body, :title
+
+  validates_length_of :body, maximum: 100000
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518155923) do
+ActiveRecord::Schema.define(:version => 20130518160903) do
 
   create_table "authors", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "twitter"
     t.string   "facebook"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
