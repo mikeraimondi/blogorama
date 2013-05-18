84644de083e4aa421daf743d477d31dc00e36042
Michael Raimondi <zeno@pobox.com>
2013-05-18 12:34:09 -0400

    Add author model with basic validations

---
+++ app/models/author.rb
@@ -0,0 +1,11 @@
+class Author < ActiveRecord::Base
+  attr_accessible :email, :facebook, :name, :twitter
+
+  validates_presence_of :name, :email
+
+  validates_uniqueness_of :email 
+  validates_format_of :email, with: /@/
+
+  validates_uniqueness_of :facebook, :twitter, allow_blank: true
+  validates_format_of :twitter, with: /^@/
+end
